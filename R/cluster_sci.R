1773d235c116884e531f2d2b0440899eacf701d6
amoneva <amonevapardo@gmail.com>
2022-08-25 18:09:37 +0200

    Replace package data and update function examples

--- R/cluster_sci.R
+++ R/cluster_sci.R
@@ -11,7 +11,7 @@
 #' @references Hart, T. C. (2019). Identifying Situational Clustering and Quantifying Its Magnitude in Dominant Case Configurations: New Methods for Conjunctive Analysis. *Crime & Delinquency, 66*(1), 143-159. https://doi.org/10.1177/0011128719866123
 #'
 #' @examples
-#' cluster_sci(cacc(data = test_data, ivs = c(iv1, iv2, iv3, iv4), dv = dv1))
+#' cluster_sci(cacc(onharassment, ivs = sex:privacy, dv = rep_victim))
 
 cluster_sci <- function (cacc_matrix) {
 
--- R/data.R
+++ R/data.R
@@ -1,11 +1,21 @@
-#' Test data to compute a Conjunctive Analysis of Case Configurations
+#' Profiles of 4174 Spanish students
 #'
-#' A dataset containing synthetic example data to conduct a Conjunctive Analysis of Case Configurations.
+#' A dataset containing the demographics, online routines, and self-reported online harassment repeat victimization and offending of 4174 Spanish non-university education students.
 #'
-#' @format A data frame with 100 rows and 9 variables:
+#' @format A data frame with 4174 rows and 12 variables:
 #' \describe{
-#'   \item{iv1, iv2, iv3, iv4}{Example integer independent variables.}
-#'   \item{iv1_c, iv2_c, iv3_c, iv4_c}{Example character or factor independent variables.}
-#'   \item{dv1}{Example integer, dichotomous, dependent variable.}
+#'   \item{sex}{`Factor`. The students' self-reported sex.}
+#'   \item{age}{`Factor`. The students' self-reported age}
+#'   \item{hours}{`Factor`. The students' self-reported number of daily hours spent online.}
+#'   \item{snapchat}{`Factor`. Whether students report using the social media Snapchat on a daily basis.}
+#'   \item{instagram}{`Factor`. Whether students report using the social media Instagram on a daily basis.}
+#'   \item{facebook}{`Factor`. Whether students report using the social media Facebook on a daily basis.}
+#'   \item{twitter}{`Factor`. Whether students report using the social media Twitter on a daily basis.}
+#'   \item{name}{`Factor`. Whether students report using their real names on social media.}
+#'   \item{photos}{`Factor`. Whether students report uploading personal photos to social media.}
+#'   \item{privacy}{`Factor`. Whether students report restricting their social media access to contacts only.}
+#'   \item{rep_victim}{`Factor`. Whether students report repeatedly suffering online harassment.}
+#'   \item{rep_offender}{`Factor`. Whether students report repeatedly committing online harassment.}
 #' }
-"test_data"
+#' @source Moneva, A., Miró-Llinares, F., & Hart, T. C. (2021). Hunter or Prey? Exploring the situational profiles that define repeated online harassment victims and offenders. *Deviant Behavior, 42*(11), 1366-1381. https://doi.org/10.1080/01639625.2020.1746135
+"onharassment"
